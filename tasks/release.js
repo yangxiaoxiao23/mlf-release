b08bccd013df89bee85704fb1cfca4d593d9c7c7
yangxiaoxiao <yangxiaoxiao23@gmail.com>
2016-01-22 13:59:36 +0800

    v0.1.5

--- tasks/release.js
+++ tasks/release.js
@@ -148,6 +148,11 @@ module.exports = function(grunt) {
 		grunt.file.write(routerJsMd5Path, fileContent);
 	}
 
+	/**
+	 * 生成成产的首页
+	 * @param  {[type]} data [description]
+	 * @return {[type]}      [description]
+	 */
 	function generateHomePage(data){
 		var srcPath = data.homePage.src;
 		var toPath = data.homePage.to;
@@ -207,6 +212,7 @@ module.exports = function(grunt) {
 		grunt.file.write(mainJsPath, fileContent);
 	}
 
+
 	grunt.registerMultiTask('release', '美丽范H5发布插件', function() {
 		var data = this.data;
 		if(this.nameArgs == 'release:test'){
@@ -229,74 +235,4 @@ module.exports = function(grunt) {
 			grunt.log.ok('复制完成');
 		}
 	});	
-		
-	/* function generateMd5File(assetName, md5){
-	}
-	
-	function getNewAssetsUrl(assetName, md5) {
-        md5=md5.substring(0,8);
-        var newurl='';
-        if(assetName.indexOf('?t=')>=0){
-            newurl = assetName.substring(0,assetName.length_8) + md5;
-        }else{
-            newurl = assetName + '?t=' + md5;
-        }
-        return newurl;
-    }
-
-    function replaceAssets(fileSrc, assetUrl) {
-        if (grunt.file.exists(fileSrc)) {
-            var data = grunt.file.read(fileSrc);
-
-            var replaceUrl = assetUrl.replaceUrl;
-
-            var path = assetUrl.path;
-
-            var assetData = grunt.file.read(path);
-
-
-            replaceUrl = replaceUrl.substring(replaceUrl.lastIndexOf('/'),replaceUrl.length);
-
-            if (data.indexOf(replaceUrl) >= 0) {
-                var md5sum = crypto.createHash('md5');
-                md5sum.update(assetData, 'utf_8');
-                var reg = new RegExp('".*' + replaceUrl + '.*"','g');
-                var fullAssetUrl = reg.exec(data).toString();
-                var assetName = fullAssetUrl.substring(fullAssetUrl.indexOf(replaceUrl),fullAssetUrl.length_1);
-                var md5 = md5sum.digest('hex');
-                var newurl = getNewAssetsUrl(assetName, md5);
-                var newdata = data.replace(assetName, newurl);
-                if (grunt.file.write(fileSrc, newdata)) {
-                    grunt.log.success(replaceUrl + ' 添加md5: ' + md5 + ' 成功');
-                } else {
-                    grunt.log.error(replaceUrl + ' 添加md5失败');
-                }
-            } else {
-                grunt.log.error('没有发现要替换的内容 ' + fileSrc);
-            }
-        }
-    }
-
-    grunt.registerMultiTask('cache', 'The best Grunt plugin ever.', function() {
-        var options = this.options({
-
-        });
-
-        var assetUrl = this.data.assetUrl;
-        this.files.forEach(function(f) {
-            var src = f.src.filter(function(filepath) {
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('源文件 "' + filepath + '" 没有找到.');
-                    return false;
-                } else {
-                    grunt.log.success('源文件 "' + filepath + '" 发现.');
-                    for(var i= 0,len=assetUrl.length;i<len;i++){
-                        var url = assetUrl[i];
-                        replaceAssets(filepath, url);
-                    }
-                    return true;
-                }
-            });
-        });
-    }); */
 };
